fad625c3cef9f0ea4f8f7b43ad9a574c1048b201
Alex <alexvnep@gmail.com>
2024-10-22 15:31:18 +0200

    docs: rename files feat:  return string that [0] is capitalised

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+import capitalize from ".";
+
+test("capitalize is defined", () => {
+  expect(capitalize).toBeDefined();
+});
+
+test("Accepts string returns same string", () => {
+  expect(capitalize("First")).toMatch("First");
+});
+
+test("Accepts string returns first letter capitalised", () => {
+  expect(capitalize("juice")).toMatch("Juice");
+});
+
+test("Accepts string returns first letter capitalised", () => {
+  expect(capitalize("my Kingdom")).toMatch("My Kingdom");
+});
